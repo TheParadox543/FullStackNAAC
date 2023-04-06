a89053a260626d60981a4727685dc946d139198f
Sam Alex Koshy <samuel.alex.koshy@gmail.com>
2023-04-06 21:51:59 +0530

    feat: create enum for better type check

--- classification/backend/main.py
+++ classification/backend/main.py
@@ -1,4 +1,5 @@
 import asyncio
+import re
 from time import perf_counter
 from typing import Optional
 
@@ -8,6 +9,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 import drivereader.drive as drive
 from drivereader.database import fetch_all_folders, fetch_all_files
+from _type import CodeValues
 
 time_now = perf_counter()
 app = FastAPI()
@@ -30,7 +32,13 @@ def read_all_folders():
     return fetch_all_folders()
 
 @app.get("/api/files")
-def read_all_files(code:Optional[str]=None, year:Optional[str]=None):
+def read_all_files(code:Optional[CodeValues]=None, year:Optional[str]=None):
+    if re.match("\d\d\d\d\-\d\d\d\d", year):
+        year1, year2 = [int(x) for x in year.split("-")]
+        if year2 != year1 + 1:
+            year = None
+    else:
+        year = None
     search = {}
     if code is not None:
         search["code"] = code
